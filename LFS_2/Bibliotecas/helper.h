de56edf758fb9a3684dd9e5030ee235803ca14a6
utnso <nicoposty@hotmail.com>
2019-06-15 12:33:16 -0300

    Bibliotecas usadas por LFS_2

---
+++ LFS_2/Bibliotecas/helper.h
@@ -0,0 +1,26 @@
+#ifndef HELPER_H_
+#define HELPER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/stat.h>
+
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/collections/list.h>
+
+
+
+#endif /* HELPER_H_ */
