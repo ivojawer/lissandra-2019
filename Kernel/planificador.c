74990086fee650e09e25ae1ef99e2e79e46ff69d
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2019-07-25 13:29:19 -0300

    Desactivado los errores en los scripts

--- Kernel/planificador.c
+++ Kernel/planificador.c
@@ -102,6 +102,8 @@ void planificadorEXEC(int idScript) {
 
 		int resultado = ejecutarRequest(requestAEjecutar, scriptEXEC);
 
+		resultado = TODO_BIEN; //TODO: Marca, esto esta temporal
+
 		sem_wait(&sem_operacionesTotales);
 		operacionesTotales++;
 		sem_post(&sem_operacionesTotales);
