e851b876ae410f8ca7efbee7c81a6efb2a4e16cf
tfloxolodeiro <37090248+tfloxolodeiro@users.noreply.github.com>
2019-08-03 07:56:36 -0300

    Mateo 11:28

--- genesis.sh
+++ genesis.sh
@@ -6,7 +6,11 @@ cd /home/utnso/workspace/tp-2019-1c-U-TN-Tecno/LFS;
 
 mkdir Debug;
 
+cp -a /home/utnso/workspace/tp-2019-1c-U-TN-Tecno/LFS/{dump.h,select.h,funcionesLFS.h,consolaLFS.c,conexionesLFS.c,create.c,conexionesLFS.h,compactacion.h,insert.c,LFS.c,funcionesLFS.c,describe.c,drop.c,dump.c,compactacion.c,select.c} /home/utnso/workspace/tp-2019-1c-U-TN-Tecno/LFS/Debug/; 
+
 cp -a /home/utnso/workspace/tp-2019-1c-U-TN-Tecno/Librerias/libreriaRequests/{conexiones.c,requests.c,colores.c,analisis.c,requests.h} /home/utnso/workspace/tp-2019-1c-U-TN-Tecno/LFS/Debug;
+
+cd /home/utnso/workspace/tp-2019-1c-U-TN-Tecno/LFS/Debug;
 
 gcc *.c -o LFS -lpthread -lcommons -lreadline;
 
